bc1b669667cef16cdc42c018887aadd58bfe1ecc
Matt Faust <mattjfaust@gmail.com>
2023-05-18 14:47:31 -0500

    Update antsforriches.sol

--- antsforriches.sol
+++ antsforriches.sol
@@ -1,78 +1,72 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract AntsForRiches {
+
     string public name = "AntsForRiches";
     string public symbol = "AFR";
-    address public owner = 0x79a621261FB4CA9a45E118E2740b42dDB6C640d3;
+    uint256 public totalSupply = 200000000;
+    uint8 public decimals = 18;
 
-    uint256 private _totalSupply = 100000000 * 10**18; // 100 million AFR
-    uint256 private _taxPercentage = 1;
-    address private _burnAddress;
+    address public owner;
 
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 
-    constructor() {
-        _balances[msg.sender] = _totalSupply;
-        _burnAddress = address(0);
-        emit Transfer(address(0), msg.sender, _totalSupply);
+    constructor(string memory n, string memory s) {
+        name = n;
+        symbol = s;
+        totalSupply = 200000000 * 10**decimals;
+        owner = msg.sender;
+        balances[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
     }
 
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(amount > 0);
+        balances[to] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), to, amount);
     }
 
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
+    function burn(uint256 amount) public onlyOwner {
+        require(amount > 0 && balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
     }
 
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        require(amount > 0, "Transfer amount must be greater than zero");
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-
-        uint256 taxAmount = (amount * _taxPercentage) / 100;
-        uint256 transferAmount = amount - taxAmount;
-
-        _balances[msg.sender] -= amount;
-        _balances[recipient] += transferAmount;
-        _balances[_burnAddress] += taxAmount;
-
-        emit Transfer(msg.sender, recipient, transferAmount);
-        emit Transfer(msg.sender, _burnAddress, taxAmount);
-
+    function transfer(address to, uint256 amount) public returns(bool success) {
+        require(amount > 0 && balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
         return true;
     }
 
-    function allowance(address _owner, address spender) public view returns (uint256) {
-        return _allowances[_owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _allowances[msg.sender][spender] = amount;
+    function approve(address spender, uint256 amount) public returns(bool success) {
+        allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        require(amount > 0, "Transfer amount must be greater than zero");
-        require(_balances[sender] >= amount, "Insufficient balance");
-        require(_allowances[sender][msg.sender] >= amount, "Allowance exceeded");
+    function allowance(address owner, address spender) public view returns(uint256) {
+        return allowances[owner][spender];
+    }
 
-        uint256 taxAmount = (amount * _taxPercentage) / 100;
-        uint256 transferAmount = amount - taxAmount;
-
-        _balances[sender] -= amount;
-        _balances[recipient] += transferAmount;
-        _balances[_burnAddress] += taxAmount;
-        _allowances[sender][msg.sender] -= amount;
-
-        emit Transfer(sender, recipient, transferAmount);
-        emit Transfer(sender, _burnAddress, taxAmount);
-
+    function transferFrom(address from, address to, uint256 amount) public returns(bool success) {
+        require(amount > 0 && allowances[from][msg.sender] >= amount);
+        allowances[from][msg.sender] -= amount;
+        balances[to] += amount;
+        balances[from] -= amount;
+        emit Transfer(from, to, amount);
         return true;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
 }
