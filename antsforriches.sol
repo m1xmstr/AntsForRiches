d696a7fa88522b3fdf71df28a8728a6d5150c170
Matt Faust <mattjfaust@gmail.com>
2023-05-19 15:21:03 -0500

    Update antsforriches.sol

--- antsforriches.sol
+++ antsforriches.sol
@@ -4,46 +4,30 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: MIT
 
 contract AntsForRiches {
-
-  // Name: AntsForRiches
-  string public name;
+  
+  string public name = "AntsForRiches"; // Name: AntsForRiches
+  string public symbol = "A4R"; // Symbol name is now: A4R
 
   // Website: www.antsforriches.com
   // GitHub: https://github.com/m1xmstr/AntsForRiches
 
-  // Total Supply: 250000000
-  uint256 public totalSupply;
+  uint256 public totalSupply; // Total Supply: 250000000
+  uint8 public decimals = 18; // Decimals: 18
 
-  // Symbol name is now: A4R
-  string public symbol;
+  address public owner = 0x79a621261FB4CA9a45E118E2740b42dDB6C640d3; // Owner
 
-  // Decimals: 18
-  uint8 public decimals = 18;
+  mapping(address => uint256) public balances; // Balances
+  mapping(address => mapping(address => uint256)) public allowances; // Allowances
 
-  // Owner
-  address public owner = 0x79a621261FB4CA9a45E118E2740b42dDB6C640d3;
-
-  // Balances
-  mapping(address => uint256) public balances;
-
-  // Allowances
-  mapping(address => mapping(address => uint256)) public allowances;
-
-  // Events
   event Transfer(address indexed from, address indexed to, uint256 amount);
   event Approval(address indexed owner, address indexed spender, uint256 amount);
 
-  // Constructor
-  // This function creates the token and initializes the initial supply.
-  constructor(string memory n, string memory s) {
-    name = n;
-    symbol = s;
+  constructor() {
     totalSupply = 250000000 * 10**uint256(decimals);
     balances[owner] = totalSupply;
     emit Transfer(address(0), owner, totalSupply);
   }
 
-  // Mint
   function mint(address to, uint256 amount) public onlyOwner {
     require(amount > 0);
     balances[to] += amount;
@@ -51,7 +35,6 @@ contract AntsForRiches {
     emit Transfer(address(0), to, amount);
   }
 
-  // Burn
   function burn(uint256 amount) public onlyOwner {
     require(amount > 0 && balances[msg.sender] >= amount);
     balances[msg.sender] -= amount;
@@ -59,7 +42,6 @@ contract AntsForRiches {
     emit Transfer(msg.sender, address(0), amount);
   }
 
-  // Transfer
   function transfer(address to, uint256 amount) public returns (bool success) {
     require(amount > 0 && balances[msg.sender] >= amount);
     balances[msg.sender] -= amount;
@@ -68,19 +50,16 @@ contract AntsForRiches {
     return true;
   }
 
-  // Approve
   function approve(address spender, uint256 amount) public returns (bool success) {
     allowances[msg.sender][spender] = amount;
     emit Approval(msg.sender, spender, amount);
     return true;
   }
 
-  // Allowance
   function allowance(address tokenOwner, address spender) public view returns (uint256) {
     return allowances[tokenOwner][spender];
   }
 
-  // TransferFrom
   function transferFrom(address from, address to, uint256 amount) public returns (bool success) {
     require(amount > 0 && allowances[from][msg.sender] >= amount);
     allowances[from][msg.sender] -= amount;
@@ -90,10 +69,8 @@ contract AntsForRiches {
     return true;
   }
 
-  // OnlyOwner modifier
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
-
 }
